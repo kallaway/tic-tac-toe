0f9f1a6fe85087b1f712679ba93d2ce05f83281a
Alexander Kallaway <kallaway@me.com>
2016-07-16 04:14:31 -0400

    set up the project

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// TODO: Maybe make it possible to generate a grid
+
+let state = [
+	[-1, -1, -1],
+	[-1, -1, -1],
+	[-1, -1, -1]
+];
+
+
+// GAME LOOP
+// 1. Choose the sign: X or O
+// - Should it randomly change what
+// 2. User makes a move
+// 3. Computer makes a move
+// When someone wins, display feedback
+// Restart the game at step 2.
